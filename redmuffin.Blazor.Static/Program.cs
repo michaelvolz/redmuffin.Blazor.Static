040dbbb49250983d012d87645b220caba3b95f30
Michael A. Volz <mvolz@redmuffin.de>
2023-11-16 13:44:01 +0100

    Simple codefix

--- redmuffin.Blazor.Static/Program.cs
+++ redmuffin.Blazor.Static/Program.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
 using redmuffin.Blazor.Static.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-await builder.Build().RunAsync();
+await builder.Build().RunAsync().ConfigureAwait(false);
