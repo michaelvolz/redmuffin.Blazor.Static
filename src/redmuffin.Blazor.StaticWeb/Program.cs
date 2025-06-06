07ec12498fc561e4b1526168b619d9e28fab5d77
Michael A. Volz <mvolz@redmuffin.de>
2025-06-06 11:29:04 +0200

    Update .editorconfig and add Blazor static web support
    - Disable specific code analysis warnings in .editorconfig.
    - Set new line and UTF-8 string literal preferences.
    - Add import for redmuffin.Blazor.StaticWeb in Program.cs.

--- src/redmuffin.Blazor.StaticWeb/Program.cs
+++ src/redmuffin.Blazor.StaticWeb/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 using redmuffin.Blazor.StaticWeb;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
